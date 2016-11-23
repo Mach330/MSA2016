d6bcfabcc6209bf90f28fc37bd59514aac5427ac
Max van duin <mvan439@aucklanduni.ac.nz>
2016-11-24 11:40:48 +1300

    Initial commit
    connected to the relevant LUIS API, startings of State Services

---
+++ Bank Bot/Bank Bot/Controllers/MessagesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Bank_Bot.Models;
+
+
+namespace Bank_Bot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                StateClient stateClient = activity.GetStateClient();
+                BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
+                LUISObj.RootObject rootObject;
+                var userMessage = activity.Text;
+
+                // Console.WriteLine(activity.Attachments[0].ContentUrl);
+
+                HttpClient client = new HttpClient();
+                string x = await client.GetStringAsync(new Uri("https://api.projectoxford.ai/luis/v2.0/apps/9dfd3e25-ebc1-41c6-955a-66f9497a9539?subscription-key=cf6eebd33695450f962472fa4215406e&q=" 
+                    + userMessage + "&timezoneOffset=12.0&verbose=true"));
+
+                rootObject = JsonConvert.DeserializeObject<LUISObj.RootObject>(x);
+
+                // return our reply to the user
+                Activity reply = activity.CreateReply($"The intent is " + rootObject.topScoringIntent.intent);
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
