22551e71a21fade52050ca1f587d3f90d2324a4e
Max van duin <mvan439@aucklanduni.ac.nz>
2016-11-25 10:32:53 +1300

    Order of operation altered
    User can now find a bank, get a greeting, send an insult or go to the website without logging in first

---
+++ AzureManager.cs
@@ -0,0 +1,114 @@
+﻿using Bank_Bot.Models;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Bank_Bot
+{
+    public class AzureManager
+    {
+
+        private static AzureManager instance;
+        private MobileServiceClient client;
+        private IMobileServiceTable<BankAccountInformation> timelineTable;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("http://maxbankbot.azurewebsites.net");
+            this.timelineTable = this.client.GetTable<BankAccountInformation>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+
+        public async Task<List<BankAccountInformation>> GetTimelines()
+        {
+            return await this.timelineTable.ToListAsync();
+        }
+
+        public async Task AddTimeline(BankAccountInformation BAInfo)
+        {
+            await this.timelineTable.InsertAsync(BAInfo);
+        }
+
+        public async Task UpdateTimeline(BankAccountInformation BAInfo)
+        {
+            await this.timelineTable.UpdateAsync(BAInfo);
+        }
+
+        public async Task<BankAccountInformation> getAccountFromName(string Name)
+        {
+            List<BankAccountInformation> accounts = await GetTimelines();
+            foreach (BankAccountInformation baI in accounts)
+            {
+                if(baI.Name.ToLower() == Name.ToLower())
+                {
+                    return baI;
+                }
+            }
+
+            return null;
+        }
+
+        public async Task<BankAccountInformation> getAccountFromNumber(string Number)
+        {
+            List<BankAccountInformation> accounts = await GetTimelines();
+            foreach (BankAccountInformation baI in accounts)
+            {
+                if (baI.accountNumber == Number)
+                {
+                    return baI;
+                }
+            }
+
+            return null;
+        }
+
+        public async Task<BankAccountInformation> payAccount(string name, string amount)
+        {
+            List<BankAccountInformation> accounts = await GetTimelines();
+            foreach (BankAccountInformation baI in accounts)
+            {
+                if(baI.Name.ToLower().Trim(' ') == name.ToLower().Trim(' ') || baI.accountNumber == name)
+                {
+                    baI.Money += Convert.ToInt32(amount);
+                    return baI;
+                }
+            }
+            return null;
+        }
+
+        public async Task<BankAccountInformation> removeFromAccount(string name, string amount)
+        {
+            List<BankAccountInformation> accounts = await GetTimelines();
+            foreach (BankAccountInformation baI in accounts)
+            {
+                if (baI.Name.ToLower().Trim(' ') == name.ToLower().Trim(' ') || baI.accountNumber == name)
+                {
+                    baI.Money -= Convert.ToInt32(amount);
+                    return baI;
+                }
+            }
+            return null;
+        }
+
+    }
+}
---
+++ Models/BankAccountInformation.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bank_Bot.Models
+{
+    public class BankAccountInformation
+    {
+
+        [JsonProperty(PropertyName = "Id")]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "money")]
+        public double Money { get; set; }
+
+        [JsonProperty(PropertyName = "accountNumber")]
+        public string accountNumber { get; set; }
+        
+        [JsonProperty(PropertyName = "bestFriend")]
+        public string bestFriend { get; set; }
+
+        [JsonProperty(PropertyName = "password")]
+        public string password { get; set; }
+    }
+}
